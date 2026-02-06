8199b7ed6ddfdd84ca75b861a43fb0c25b23bef7
vortiz <colombia2002titi@gmail.com>
2026-02-06 13:08:05 +0000

    last-first-kiss

---
+++ last-first-kiss.js
@@ -0,0 +1,18 @@
+
+// Return first element 
+function first(arr){
+
+    return value[0];
+}
+
+// Return last elememt 
+function last(str){
+    return value[value.length -1];
+}
+
+// Returns an array of 2 elements: last element and first, in that order.
+function kiss(array){
+    return [value[value.length -1], value[0]];
+
+
+}
