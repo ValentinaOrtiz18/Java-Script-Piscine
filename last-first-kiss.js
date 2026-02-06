3e3e8434c6331a2fd5532f490f9e1913c47da561
vortiz <colombia2002titi@gmail.com>
2026-02-06 13:25:43 +0000

    last-first-kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -2,17 +2,23 @@
 // Return first element 
 function first(arr){
 
-    return value[0];
+    return arr[0];
 }
 
 // Return last elememt 
 function last(str){
-    return value[value.length -1];
+    return str[str.length -1];
 }
 
 // Returns an array of 2 elements: last element and first, in that order.
-function kiss(array){
-    return [value[value.length -1], value[0]];
+function kiss(arr){
+    return [arr[arr.length -1], arr[0]];
 
 
 }
+
+/* Testing it works 
+
+test = [1,3,'6',8,'0']
+console.log(kiss(test))
+*/
