e37e6713b58bbbeb2c668486b1ad67e863904bd6
vortiz <colombia2002titi@gmail.com>
2026-02-08 19:09:12 +0000

    functions that get and edit from objects

--- change.js
+++ change.js
@@ -1,31 +1,54 @@
 
-       
-  /*     
+ // Function that takes a key and returns its corresponding value from only sourceObject
+function get(key){
+    return sourceObject[key];
+
+};
+
+// Function that takes a key and a value, updates or adds keys and/or values 
+ function set(key, value){
+    sourceObject[key] = value;
+    return value;
+   
+};
+ 
+ 
+// MY VERSION 
+ 
+ /*      
+  
        const sourceObject = {
   num: 42,
   bool: true,
   str: 'some text',
   log: console.log,
 };
-  */
 
-// function that takes a key and returns its corresponding value 
-function get(key,sourceObject){
-    return sourceObject[key];
+       const valentest = {
+  age: 23,
+  name: 'valentina',
+  ciudad: 'londres',
+};
+
+
+// Generic function that takes a key and returns its corresponding value from any obj
+function get(key, obj){
+    return obj[key];
 
 };
-// Function that takes and key and a value, updates the value for the corresponding property and lastly, returns the value 
- function set(key, value ){
-    sourceObject[key] = value;
+
+// Generic function that takes a key and a value, updates or adds keys and/or values 
+ function set(key, value, obj){
+    obj[key] = value;
     return value;
    
 };
 
-/*
-set('bool', false);
- console.log(get('bool', sourceObject));
-*/
+ 
+set('funciona', 'tambien', valentest );
+console.log(valentest);
 
+*/
 
 
 
