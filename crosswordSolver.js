4e65b29f7e967833e8b6c94f4259bd3cac932d30
vortiz <colombia2002titi@gmail.com>
2026-02-09 17:12:05 +0000

    dog year in other planets

---
+++ crosswordSolver.js
@@ -0,0 +1,7 @@
+function crosswordSolver(emptyPuzzle, wordList){
+
+    // each character will be either a number, a . or a \n;
+
+    // a number represents the number of words starting from the specific position and a . represents a space that does not need to be filled.
+    const emptyPuzzle = ''
+}
