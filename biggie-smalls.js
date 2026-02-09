d5d6ceda388e8ba39e38e3cf4156162886d0367d
vortiz <colombia2002titi@gmail.com>
2026-02-09 17:46:52 +0000

    smaller and bigger possible number

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,3 +1,3 @@
-const smalls = Infinity; // smaller than the min possible value 
+const smalls = -Infinity; // smaller than the min possible value 
 const biggie = Infinity; // larger than the max possible value 
 // console.log(smalls, biggie)
